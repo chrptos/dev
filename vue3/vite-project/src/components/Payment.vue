b365b1bf9ce1a83baeadca5c17ffdee4452c809c
chrptos <chrptos@gmail.com>
2023-05-23 22:31:18 +0900

    v-model

--- vue3/vite-project/src/components/Payment.vue
+++ vue3/vite-project/src/components/Payment.vue
@@ -1,20 +1,29 @@
 <script setup lang="ts">
-const itemName1 = 'Desk';
-const price1 = 400;
+import { reactive } from 'vue'
+
 const url = 'https://www.amazon.co.jp/ref=nav_logo';
-const buy = () => {
-    alert('購入されました。');
+
+let item = reactive({
+    name: 'Desk',
+    price: 40000
+})
+
+const buy = (item: any) => {
+    alert(item.name + '購入しました。');
 }
+
 </script>
 
 <template>
     <div class="container">
         <h1>最近の支出</h1>
+        <input type="text" v-model="item.name">
+        <input type="text" v-model="item.price">
         <div class="payment">
-            <label for="">{{ itemName1 }}</label>
-            <label for="">{{ price1 }}円</label>
+            <label for="">{{ item.name }}</label>
+            <label for="">{{ item.price }}円</label>
             <a v-bind:href="url">リンク</a>
-            <button v-on:click="buy">購入</button>
+            <button v-on:click="buy(item)">購入</button>
         </div>
     </div>
 </template>
