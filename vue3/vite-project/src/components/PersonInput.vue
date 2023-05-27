2074ab69a7cf5b8ed4104058334db8fa63af4406
chrptos <chrptos@gmail.com>
2023-05-27 21:02:06 +0900

    add validation

--- vue3/vite-project/src/components/PersonInput.vue
+++ vue3/vite-project/src/components/PersonInput.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, defineEmits } from 'vue';
+import { ref, defineEmits, computed } from 'vue';
 
 const emit = defineEmits(['registerPerson']);
 
@@ -11,16 +11,28 @@ const registerPerson = () => {
     emit('registerPerson', person);
 }
 
+const nameLengthLimit: number = 15;
+const isValidName = computed(() => {
+    return inputName.value.length <= nameLengthLimit ? true : false;
+});
+const color = computed(() => {
+    return isValidName.value ? 'black' : 'red';
+});
+
 </script>
 
 <template>
     <div class="person-form">
         <span for="name">名前：</span>
-        <input type="text" v-model="inputName">
+        <input class="input__name" type="text" v-model="inputName">
         <span for="age">年齢：</span>
-        <input type="number" name="age" v-model="inputAge">
-        <button @click="registerPerson">送信</button>
+        <input class="input__age" type="number" name="age" v-model="inputAge">
+        <button :disabled="!isValidName" @click="registerPerson">送信</button>
     </div>
 </template>
 
-<style></style>
+<style>
+input {
+    background-color: v-bind(color);
+}
+</style>
