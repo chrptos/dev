b7a711e5efa35817da19001c36ab7f0399493f45
chrptos <chrptos@gmail.com>
2023-05-31 19:54:28 +0900

    add: async api example

--- nuxt3/sample/pages/index.vue
+++ nuxt3/sample/pages/index.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
         <h1>top page</h1>
+        <ul>
+            <li v-for="user in users" :key="user.id">
+                {{ user.id }}, {{ user.name }}
+            </li>
+        </ul>
     </div>
 </template>
+<script setup>
+const url = 'https://jsonplaceholder.typicode.com/users';
+const { data: users } = await useFetch(url);
+console.log(users);
+</script>
