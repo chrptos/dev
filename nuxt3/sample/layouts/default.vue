78271dcb2b5ffc4d3cb5c64baf86d88d99ae8f4b
chrptos <chrptos@gmail.com>
2023-05-31 19:45:31 +0900

    add: default layout

---
+++ nuxt3/sample/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <slot />
+        <footer>&copy; footerをここに入力</footer>
+    </div>
+</template>
+
+<style>
+html {
+    font-family: 'Monomaniac One', sans-serif;
+}
+
+.container {
+    color: red;
+}
+
+footer {
+    background-color: orange;
+    text-align: center;
+    color: white;
+}
+</style>
