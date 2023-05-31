19c7131832a056309f282971634e2603b55a48d0
chrptos <chrptos@gmail.com>
2023-05-31 20:55:39 +0900

    add: useState

---
+++ nuxt3/sample/composables/index.ts
@@ -0,0 +1,12 @@
+import {Ref } from 'vue';
+
+export const useTitle = () => {
+    const title = useState<string>('title', ()=>'Hello');
+    const changeTitle = (title: Ref<string>) => (value: string) => {
+        title.value = value
+    }
+    return {
+        title: readonly(title),
+        changeTitle: changeTitle(title)
+    }
+}
--- nuxt3/sample/pages/state.vue
+++ nuxt3/sample/pages/state.vue
@@ -2,9 +2,11 @@
     <div>
         <h1>state page</h1>
         <h2>{{ title }}</h2>
+        <button @click="titleState.changeTitle('Next3')">Nuxt3</button>
     </div>
 </template>
 
 <script setup>
-const title = useTitle();
+const titleState = useTitle();
+const { title } = titleState;
 </script>
