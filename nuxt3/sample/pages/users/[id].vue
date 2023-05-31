b8da117d6173ba8590fea780cc7d8bbe90768e4a
chrptos <chrptos@gmail.com>
2023-05-31 19:24:39 +0900

    add: page validation

---
+++ nuxt3/sample/pages/users/[id].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>page {{ $route.params.id }}</h1>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    validate: async (route) => {
+        return /^\d+$/.test(route.params.id);
+    }
+})
+</script>
