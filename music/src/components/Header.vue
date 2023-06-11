7c33b9cd1e81565912533b237e79a7a0adf44842
chrptos <chrptos@gmail.com>
2023-06-11 22:15:33 +0900

    init

---
+++ music/src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <!-- Header -->
+  <header id="header" class="bg-gray-700">
+    <nav class="container mx-auto flex justify-start items-center py-5 px-4">
+      <!-- App Name -->
+      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+
+      <div class="flex flex-grow items-center">
+        <!-- Primary Navigation -->
+        <ul class="flex flex-row mt-1">
+          <!-- Navigation Links -->
+          <li>
+            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
+              >Login / Register</a
+            >
+          </li>
+          <li>
+            <a class="px-2 text-white" href="#">Manage</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+import { mapStores } from "pinia";
+import useModalStore from "@/stores/modal";
+
+export default {
+  name: "AppHeader",
+  computed: {
+    ...mapStores(useModalStore),
+    /**
+     *
+     * {
+     *   modalStore: {
+     *     isOpen: computed(() => store.state.isOpen), // the current value of the 'isOpen' state
+     *     hiddenClass: computed(() => store.getters.hiddenClass), // the current value of the 'hiddenClass' getter
+     *   }
+     * }
+     *
+     * 特定のstateのみ必要である場合、
+     * ...mapState(useModalStore, ["hiddenClass"]),
+     */
+  },
+  methods: {
+    toggleAuthModal() {
+      this.modalStore.isOpen = !this.modalStore.isOpen;
+      console.log(this.modalStore.isOpen);
+    },
+  },
+};
+</script>
