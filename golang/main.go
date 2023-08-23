4d2a1f6bcaf1450efaba671a1afa973fdf6d62d8
chrptos <chrptos@gmail.com>
2023-08-23 20:22:45 +0900

    add page

--- golang/main.go
+++ golang/main.go
@@ -5,14 +5,24 @@ import (
 	"net/http"
 )
 
+const portNumber string = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!!")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "2 + 2 is %d", addValues(2, 2))
+}
+
+func addValues(a, b int) int {
+	return a + b
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, World!")
-		if err != nil {
-			fmt.Println("err")
-		}
-		// fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-		fmt.Printf("Number of bytes written: %d\n", n)
-	})
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Printf("Starting application on port%s", portNumber)
+	http.ListenAndServe(portNumber, nil)
 }
