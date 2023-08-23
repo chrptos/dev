f90eda004588626da966dd0cad0fc4a181cf3254
chrptos <chrptos@gmail.com>
2023-08-23 21:32:08 +0900

    add html render

--- golang/main.go
+++ golang/main.go
@@ -1,47 +1,32 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 const portNumber string = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!!")
+	renderTemplate(w, "home.page.tmpl")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "2 + 2 is %d", addValues(2, 2))
+	renderTemplate(w, "about.page.tmpl")
 }
 
-func Devide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValues(100.0, 0.0)
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedFile, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedFile.Execute(w, nil)
 	if err != nil {
-		fmt.Fprint(w, err)
-	} else {
-		fmt.Fprintf(w, "100 / 100 is %f", f)
-	}
-}
-
-func addValues(a, b int) int {
-	return a + b
-}
-
-func divideValues(a, b float32) (float32, error) {
-	if b == 0 {
-		err := errors.New("cannot devide by 0")
-		return 0, err
-	} else {
-		return a / b, nil
+		fmt.Println(err)
 	}
 }
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Devide)
 
 	fmt.Printf("Starting application on port%s", portNumber)
 	http.ListenAndServe(portNumber, nil)
