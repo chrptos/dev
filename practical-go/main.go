1a1073187bd9431a81a0d873177e04e439f8fd95
chrptos <chrptos@gmail.com>
2023-09-05 21:54:52 +0900

    cla

--- practical-go/main.go
+++ practical-go/main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"os"
 )
 
 func getName(r io.Reader, w io.Writer) (string, error) {
@@ -14,11 +15,20 @@ func getName(r io.Reader, w io.Writer) (string, error) {
 	scanner := bufio.NewScanner(r)
 	scanner.Scan()
 	if err := scanner.Err(); err != nil {
-			return "", err
+		return "", err
 	}
 	name := scanner.Text()
 	if len(name) == 0 {
-			return "", errors.New("You didn't enter your name")
+		return "", errors.New("You didn't enter your name")
 	}
 	return name, nil
+}
+
+func main() {
+	name, err := getName(os.Stdin, os.Stdout)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "An error occurred: %v\n", err)
+		return
+	}
+	fmt.Printf("Hello, %s!\n", name)
 }
